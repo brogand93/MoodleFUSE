4df04260f8e3fc6bd482821c23e702977b0896ce
BroganD1993 <darrenbrogan@hotmail.com>
2015-02-04 17:48:48 +0000

    Add empty database revision

---
+++ migrations/versions/37120a8c2ae4_initialize.py
@@ -0,0 +1,24 @@
+"""initialize
+
+Revision ID: 37120a8c2ae4
+Revises: 
+Create Date: 2015-02-04 17:48:16.009339
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '37120a8c2ae4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
