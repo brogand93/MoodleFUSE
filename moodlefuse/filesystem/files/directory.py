2f176dd9a3aa532a2de6b6a57ca84541860869ca
BroganD1993 <darrenbrogan@hotmail.com>
2015-04-10 17:30:37 +0100

    Fix build

--- moodlefuse/filesystem/files/directory.py
+++ moodlefuse/filesystem/files/directory.py
@@ -3,14 +3,12 @@
 
 from stat import S_IFDIR, S_IREAD, S_IWRITE
 from moodlefuse.filesystem.files.file import File
-from fuse import fuse_get_context
 
 
 class Directory(File):
 
     def __init__(self):
         super(Directory, self).__init__()
-        uid, gid, _ = fuse_get_context()
         self.attrs['st_mode'] = (S_IFDIR | S_IREAD | S_IWRITE)
         self.attrs['st_size'] = 4096
 
