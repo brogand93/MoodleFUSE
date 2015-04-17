6bc313ad490d45810171cd64944e210219b6a2fb
BroganD1993 <darrenbrogan@hotmail.com>
2015-04-17 20:54:20 +0100

    Fix browser headers

--- moodlefuse/moodle/emulator/core_emulator.py
+++ moodlefuse/moodle/emulator/core_emulator.py
@@ -20,10 +20,12 @@ class CoreEmulator(object):
     def setup_emulator(self):
         self.browser = Browser()
         self.browser.addheaders = [
-            (
-                'User-agent',
-                'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'
-            )
+            ('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'),
+            ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+            ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'),
+            ('Accept-Encoding', 'none'),
+            ('Accept-Language', 'en-US,en;q=0.8'),
+            ('Connection', 'keep-alive')
         ]
         self.cookiejar = CookieJar()
         self.browser.set_cookiejar(self.cookiejar)
