b986861ef9c246f41035965482020e1eb3913fe8
BroganD1993 <darrenbrogan@hotmail.com>
2015-04-18 13:12:31 +0100

    Ignore robots.txt

--- moodlefuse/moodle/emulator/core_emulator.py
+++ moodlefuse/moodle/emulator/core_emulator.py
@@ -19,6 +19,7 @@ class CoreEmulator(object):
 
     def setup_emulator(self):
         self.browser = Browser()
+        self.browser.set_handle_robots(False)
         self.browser.addheaders = [
             ('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.132 Safari/537.36'),
             ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
